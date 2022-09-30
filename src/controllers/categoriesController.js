d7fc6249c2c2cc4f20d1c81fa927dc9cc3b9f6fb
Guilherme <guilhermepolicarpio@gmail.com>
2022-09-30 10:19:56 -0300

    Categories controller ajustments

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -4,6 +4,11 @@ export async function addCategories(req, res){
     const { name } = req.body;
     
     try{
+        const repeated = await connection.query(`SELECT * FROM categories WHERE name = $1`, [name]);
+        if(repeated.rows.length>0){
+            console.log("eroouu")
+            return res.status(409).send("Categoria já existente");
+        }
         await connection.query(`INSERT INTO categories (name) VALUES ($1);`, [name]);
         res.sendStatus(201);
     }catch (err){
