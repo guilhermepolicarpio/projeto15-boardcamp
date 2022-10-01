2280fcf761d276f14844eeba7b19fdb68bf244e3
Guilherme <guilhermepolicarpio@gmail.com>
2022-10-01 09:26:41 -0300

    Implementation of adding rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,1 +1,25 @@
-import connection from "../database";
+import connection from "../database.js";
+import dayjs from "dayjs";
+
+export async function addRentals(req,res){
+    const rentDate = dayjs(Date.now()).format("DD/MM/YYYY");
+    const { customerId, gameId, daysRented} = req.body;
+
+    const game = await connection.query(`SELECT * FROM games WHERE id=$1`, [gameId])
+    const {pricePerDay} = game.rows[0];
+    const originalPrice = pricePerDay*daysRented;
+
+    try{
+        await connection.query(`INSERT INTO rentals 
+        ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice","delayFee")
+        VALUES ($1, $2, $3, $4, $5, $6, $7)
+        `, [customerId, gameId, rentDate, daysRented, null, originalPrice, null])
+        res.sendStatus(201);
+        
+
+    } catch(err){
+        console.log(err)
+        return res.sendStatus(500);
+    } 
+
+}
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,5 +1,8 @@
 import { Router} from "express";
+import { addRentals } from "../controllers/rentalsController.js";
 
 const rentalsRouter = Router();
+
+rentalsRouter.post("/rentals", addRentals)
 
 export default rentalsRouter;
