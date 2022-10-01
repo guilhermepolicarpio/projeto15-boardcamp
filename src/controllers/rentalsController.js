0bec996c802b9ee5c04798b0f2a77d0a459d2555
Guilherme <guilhermepolicarpio@gmail.com>
2022-10-01 09:41:13 -0300

    Implementation of deleting rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -20,6 +20,24 @@ export async function addRentals(req,res){
     } catch(err){
         console.log(err)
         return res.sendStatus(500);
-    } 
-
+    }
 }
+
+    export async function deleteRentals(req,res){
+
+        const {id} = req.params;
+
+        try{
+            const exist = await connection.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
+            if(exist.rows.length === 0){
+            
+                return res.status(404).send("Aluguel não existe");
+            }
+            
+            await connection.query(`DELETE FROM rentals WHERE id=$1`, [id])
+            return res.sendStatus(200);
+
+        }catch (err){
+        return res.sendStatus(500);
+        }
+    }
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,8 +1,8 @@
 import { Router} from "express";
-import { addRentals } from "../controllers/rentalsController.js";
+import { addRentals,deleteRentals } from "../controllers/rentalsController.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.post("/rentals", addRentals)
-
+rentalsRouter.delete("/rentals/:id", deleteRentals)
 export default rentalsRouter;
